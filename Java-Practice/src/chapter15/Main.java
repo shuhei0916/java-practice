ff5562cd87a1ecb33de198e719db064bb2a371c8
shuhei0916 <shuheiito.stc@gmail.com>
2024-06-21 14:20:16 +0900

    592ページまで終了。

--- Java-Practice/src/chapter15/Main.java
+++ Java-Practice/src/chapter15/Main.java
@@ -1,25 +1,85 @@
 package chapter15;
+import java.time.Instant;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.util.Calendar;
+import java.util.Date;
 
 public class Main {
 	public static void main(String[] args) {
-		String s1 = "すっきりJava";
-		String s2 = "Java";
-		String s3 = "java";
+//		String s1 = "すっきりJava";
+//		String s2 = "Java";
+//		String s3 = "java";
+//		
+//		if (s2.equals(s3)) {
+//			System.out.println("s2 equals to s3");
+//		}
+//		if (s2.equalsIgnoreCase(s3)) {
+//			System.out.println("s2とs3はケースを区別しなければ等しい");
+//		}
+//		System.out.println("s1の長さは" + s1.length());
+//		System.out.println(s1.substring(1, 3));
+//		
+//		StringBuilder sb = new StringBuilder();
+//		for (int i = 0; i < 100; i++) {
+//			sb.append("Java");
+//		}
+//		String s = sb.toString();
+//		System.out.println(s);
+//		
+//		String s4 = "abc,def:ghi";
+//		String[] words = s4.split("[,:]");
+//		for (String w : words) {
+//			System.out.print(w + "->");
+//		}
 		
-		if (s2.equals(s3)) {
-			System.out.println("s2 equals to s3");
-		}
-		if (s2.equalsIgnoreCase(s3)) {
-			System.out.println("s2とs3はケースを区別しなければ等しい");
-		}
-		System.out.println("s1の長さは" + s1.length());
-		System.out.println(s1.substring(1, 3));
+//		Date now = new Date();
+//		System.out.println(now);
+//		System.out.println(now.getTime());
+//		Date past = new Date(150000L);
+//		System.out.println(past);
 		
-		StringBuilder sb = new StringBuilder();
-		for (int i = 0; i < 100; i++) {
-			sb.append("Java");
-		}
-		String s = sb.toString();
-		System.out.println(s);
+		Calendar c = Calendar.getInstance();
+		c.set(2019, 8, 22, 1, 23, 45);
+		Date d = c.getTime();
+		System.out.println(d);
+		
+		Date now = new Date();
+		c.setTime(now);
+		int y = c.get(Calendar.YEAR);
+		System.out.println("今年は" + y + "年です");
+		
+		Instant i1 = Instant.now();
+		
+		Instant i2 = Instant.ofEpochMilli(160000004L);
+		long l = i2.toEpochMilli();
+		
+		ZonedDateTime z1 = ZonedDateTime.now();
+		ZonedDateTime z2 = ZonedDateTime.of(2020,  1, 2, 3, 4, 5, 6, ZoneId.of("Asia/Tokyo"));
+		System.out.println("Tokyo: " + z2.getYear() + z2.getMonth() + z2.getDayOfMonth());
+		
+		
+//		Instant i3 = z2.toInstant();
+//		ZonedDateTime z3 = i3.atZone(:)
+		
 	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
