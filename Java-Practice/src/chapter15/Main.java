47c5b4eb343c32e4d5368e0c3de29e2bf726dac0
shuhei0916 <shuheiito.stc@gmail.com>
2024-06-22 23:37:55 +0900

    p624まで完了。

--- Java-Practice/src/chapter15/Main.java
+++ Java-Practice/src/chapter15/Main.java
@@ -2,8 +2,6 @@ package chapter15;
 import java.time.Instant;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
-import java.util.Calendar;
-import java.util.Date;
 
 public class Main {
 	public static void main(String[] args) {
@@ -39,15 +37,15 @@ public class Main {
 //		Date past = new Date(150000L);
 //		System.out.println(past);
 		
-		Calendar c = Calendar.getInstance();
-		c.set(2019, 8, 22, 1, 23, 45);
-		Date d = c.getTime();
-		System.out.println(d);
-		
-		Date now = new Date();
-		c.setTime(now);
-		int y = c.get(Calendar.YEAR);
-		System.out.println("今年は" + y + "年です");
+//		Calendar c = Calendar.getInstance();
+//		c.set(2019, 8, 22, 1, 23, 45);
+//		Date d = c.getTime();
+//		System.out.println(d);
+//		
+//		Date now = new Date();
+//		c.setTime(now);
+//		int y = c.get(Calendar.YEAR);
+//		System.out.println("今年は" + y + "年です");
 		
 		Instant i1 = Instant.now();
 		
@@ -56,12 +54,22 @@ public class Main {
 		
 		ZonedDateTime z1 = ZonedDateTime.now();
 		ZonedDateTime z2 = ZonedDateTime.of(2020,  1, 2, 3, 4, 5, 6, ZoneId.of("Asia/Tokyo"));
-		System.out.println("Tokyo: " + z2.getYear() + z2.getMonth() + z2.getDayOfMonth());
 		
 		
-//		Instant i3 = z2.toInstant();
-//		ZonedDateTime z3 = i3.atZone(:)
 		
+//		System.out.println("Tokyo: " + z2.getYear() + z2.getMonth() + z2.getDayOfMonth());
+		
+		
+		Instant i3 = z2.toInstant();
+		ZonedDateTime z3 = i3.atZone(ZoneId.of("Europe/London"));
+		
+		// ZonedDataTimeの利用方法
+		System.out.println("東京: " + z2.getYear() + z2.getMonth() + z2.getDayOfMonth()+ ", " + z3.getHour() + ":" + z3.getMinute());
+		System.out.println("ロンドン: " + z3.getYear() + z3.getMonth() + z3.getDayOfMonth() + ", " + z3.getHour() + ":" + z3.getMinute());
+		
+		if (z2.isEqual(z3)) {
+			System.out.println("これらは同じ瞬間を指しています");
+		}
 	}
 }
 
---
+++ Java-Practice/src/chapter16/Main.java
@@ -0,0 +1,23 @@
+package chapter16;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class Main {
+	public static void main(String[] args) {
+		ArrayList<Integer> points = new ArrayList<Integer>();
+		points.add(10);
+		points.add(80);
+		points.add(75);
+		for (int i: points) {
+			System.out.println(i);
+		}
+		
+		Iterator<Integer> it = points.iterator();
+		while(it.hasNext()) {
+			Integer i = it.next();
+			System.out.println(i);
+		}
+		
+	}
+}
