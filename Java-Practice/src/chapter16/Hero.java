4a75b3ee7ac1fa27d5a9eb5b20d26c12b6fe8684
shuhei0916 <shuheiito.stc@gmail.com>
2024-06-23 17:19:58 +0900

    16章まで終了。

---
+++ Java-Practice/src/chapter16/Hero.java
@@ -0,0 +1,7 @@
+package chapter16;
+
+public class Hero {
+	public String name;
+	public Hero(String name) { this.name = name; }
+	public String getName() { return this.name; }
+}
--- Java-Practice/src/chapter16/Main.java
+++ Java-Practice/src/chapter16/Main.java
@@ -1,23 +1,37 @@
 package chapter16;
 
 import java.util.ArrayList;
-import java.util.Iterator;
 
 public class Main {
 	public static void main(String[] args) {
-		ArrayList<Integer> points = new ArrayList<Integer>();
-		points.add(10);
-		points.add(80);
-		points.add(75);
-		for (int i: points) {
-			System.out.println(i);
-		}
+//		ArrayList<Integer> points = new ArrayList<Integer>();
+//		points.add(10);
+//		points.add(80);
+//		points.add(75);
+//		for (int i: points) {
+//			System.out.println(i);
+//		}
+//		
+//		Iterator<Integer> it = points.iterator();
+//		while(it.hasNext()) {
+//			Integer i = it.next();
+//			System.out.println(i);
+//		}
+
+//		Set<String> colors = new HashSet<String>();
+//		
+//		colors.add("Red");
+//		colors.add("Blue");
+//		colors.add("Yellow");
+//		colors.add("Red");
 		
-		Iterator<Integer> it = points.iterator();
-		while(it.hasNext()) {
-			Integer i = it.next();
-			System.out.println(i);
-		}
+		ArrayList<Hero> heroes = new ArrayList<Hero>();
+		Hero suzuki = new Hero("suzuki");
+		heroes.add(suzuki);
 		
+		for (Hero h : heroes) {
+			System.out.println(h);
+		}
+	
 	}
 }
