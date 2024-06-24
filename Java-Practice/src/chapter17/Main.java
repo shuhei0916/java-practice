35bad4680ab75fde50aaf540ff3fb1be9be12262
shuhei0916 <shuheiito.stc@gmail.com>
2024-06-24 14:27:00 +0900

    第17章まで完了。

--- Java-Practice/src/chapter17/Main.java
+++ Java-Practice/src/chapter17/Main.java
@@ -1,5 +1,13 @@
 package chapter17;
 
-public class Main {
+import java.io.FileWriter;
 
+public class Main {
+	public static void main(String[] args) {
+		try (FileWriter fw = new FileWriter("data.txt");) {
+			fw.write("hello");
+		} catch (Exception e) {
+			System.out.println("Error" + e);
+		}
+	}
 }
